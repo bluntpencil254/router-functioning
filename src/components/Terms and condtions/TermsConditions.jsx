63d73523b7280eb210344f6c0ba8ec4e68350c76
bluntpencil254 <makorimike9@gmail.com>
2024-12-25 23:08:04 +0300

    added content for terms and conditions

---
+++ src/components/Terms and condtions/TermsConditions.jsx
@@ -0,0 +1,138 @@
+import React from 'react'
+
+function TermsConditions() {
+    
+  return (
+    <div className='text-gray-600 mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8'>
+        <h1 className='text-center font-bold p-3 '>TERMS AND CONDITIONS</h1>
+
+<p className='text-end p-4'>
+   Effective Date: 16.12.2024 <br />
+   Last Updated: 16.12.2024
+</p>
+
+Welcome to <em>SwiftInbox</em>! By using our website or app (collectively, the "Service"), you agree to the following Terms and Conditions. Please read them carefully. If you do not agree, you must not use the Service.
+
+<h2 className='text-orange-700 font-bold p-3'>1. Acceptance of Terms  </h2>
+<p>
+By accessing or using the Service, you confirm that you are at least 13 years old and capable of entering into a legally binding agreement. If you are using the Service on behalf of a business or organization, you confirm you have the authority to bind them to these Terms.
+</p>
+
+<h2 className='text-orange-700 font-bold p-3'>2. Changes to the terms</h2>
+We reserve the right to update these Terms at any time. Changes will be effective upon posting. It is your responsibility to review the Terms regularly. Continued use of the Service signifies acceptance of any modifications.
+
+<h2 className='text-orange-700 font-bold p-3'>3. User Account</h2>
+
+<p><strong>a.</strong> 
+Registration: To access certain features, you must create an account with accurate and current information.  
+</p>
+<p><strong>b.</strong> 
+Registration: To access certain features, you must create an account with accurate and current information.  
+</p>
+<p><strong>c.</strong> 
+Account Security: You are responsible for maintaining the confidentiality of your account credentials. Notify us immediately of unauthorized access.
+</p>
+<p><strong>d.</strong>
+Eligibility: Accounts may not be transferred, sold, or shared without our explicit consent.
+</p> 
+
+
+<h2 className='text-orange-700 font-bold p-3'>4. Permited use</h2>
+ <p><strong>a. </strong>
+You agree to use the Service solely for lawful purposes and in compliance with these Terms and applicable laws.
+</p>
+<p><strong>b. </strong>Prohibited uses include, but are not limited to:</p>
+<ul><li>
+- Sending spam or unsolicited emails. <br /> 
+   - Engaging in fraud, phishing, or any illegal activity.  <br />
+   - Uploading harmful or malicious software. <br />
+    </li>
+</ul> 
+<p className='text-red-600'><strong>c. </strong>
+Violations may result in account suspension or termination.</p> 
+
+<h2 className='text-orange-700 font-bold p-3'>5. Service Features</h2>
+<p><strong>a. </strong> Email Accounts: We provide email accounts with specified storage limits. Excessive use may result in additional charges or restrictions.
+</p>
+<p><strong>b. </strong>Data Backup: While we strive to secure your data, we are not responsible for loss due to technical errors. Regularly back up your emails.
+</p>
+<p><strong>c. </strong>
+Advertisements: Some features may display third-party advertisements, subject to our [Privacy Policy]. 
+{/* remember to add the link to privacy policy */}
+</p>
+
+<h2 className='text-orange-700 font-bold p-3'>6. Fees and payments</h2>
+<p><strong>a. </strong>Certain premium features require payment. All fees are listed on the Service and must be paid in advance.
+</p>
+<p><strong>b. </strong>
+Subscription services will renew automatically unless canceled.
+</p>
+<p><strong>c. </strong>
+Refunds are only available as outlined in our Refund Policy.
+</p>
+
+<h2 className='text-orange-700 font-bold p-3'>7. Intellectual property</h2>
+<p><strong>a. </strong>Ownership: All content, trademarks, and code in the Service are owned by us or our licensors.
+</p>
+<p><strong>b. </strong>
+License: We grant you a non-exclusive, non-transferable license to use the Service.
+</p>
+<p><strong>b. </strong>
+Restrictions: You may not copy, modify, distribute, or reverse-engineer any part of the Service.
+</p>
+
+<h2 className='text-orange-700 font-bold p-3'>8. Privacy of data</h2>
+<p><strong>a. </strong>
+Your use of the Service is subject to our [Privacy Policy].
+{/* remember to put the link to the privacy policy */}
+</p>
+<p><strong>b. </strong>
+We may process personal data as necessary to provide the Service, including compliance with legal obligations.
+</p>
+<p><strong>c. </strong>By using the Service, you consent to data processing as described in the Privacy Policy.
+</p>
+
+<h2 className='text-orange-700 font-bold p-3'>9. Third party Intergrations</h2>
+<p><strong>a. </strong>The Service may integrate with third-party applications. We are not responsible for their actions or terms. 
+</p>
+<p><strong>c. </strong>Any third-party services used are governed by their respective terms.
+</p>
+
+<h2 className='text-orange-700 font-bold p-3'>10. Termination</h2>
+<p><strong>a. </strong>We reserve the right to suspend or terminate your account at our discretion, 
+including but not limited to breaches of these Terms. </p>
+<p><strong>b. </strong> 
+You may terminate your account by contacting support. Upon termination, your data may be deleted permanently.</p>
+
+<h2 className='text-orange-700 font-bold p-3'>11. Limitation and liability</h2>
+<p><strong>a. </strong>
+To the fullest extent permitted by law, we are not liable for any indirect, incidental, 
+or consequential damages arising from your use of the Service.</p>
+<p><strong>a. </strong> 
+Our total liability for claims related to the Service shall not exceed the amount you paid to us in the past 12 months.</p>
+
+<h2 className='text-orange-700 font-bold p-3'>12. Indemnification</h2>
+You agree to indemnify and hold harmless <em>SwifInbox</em>, its affiliates, employees, and agents from any claims, damages,
+ or expenses arising from your use of the Service or breach of these Terms.
+
+<h2 className='text-orange-700 font-bold p-3'>13. Dispute Resolution</h2>
+<p><strong>a. </strong>
+Governing Law: These Terms are governed by the laws of Kenya.</p>
+<p><strong>b. </strong>Arbitration: Disputes shall be resolved through binding arbitration, except where prohibited by law.</p>
+<p><strong>c. </strong>Class Action Waiver: You agree not to participate in any class action related to the Service.</p>
+
+<h2 className='text-orange-700 font-bold p-3'>14. General Provisions</h2> 
+<p><strong>a. </strong>Entire Agreement: These Terms constitute the entire agreement between you and us. </p>
+<p><strong>b. </strong>Severability: If any part of these Terms is found to be unenforceable, the remaining provisions shall remain in effect.</p>
+<p><strong>c. </strong>Waiver: Failure to enforce any part of these Terms does not constitute a waiver of our rights.</p>
+
+<h2 className='text-orange-700 font-bold p-3'>15. Contact Information </h2>
+For questions about these Terms, please contact us at: <br /> 
+<em><a href="gmail.com">makorimike9@gmail.com</a></em> <br />
+<p><em>56709 - 600, Nairobi</em></p>
+<p>0711906157</p>  
+    </div>
+  )
+}
+
+export default TermsConditions
--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,14 @@ import About from './components/About/About.jsx'
 import User from './components/User/User.jsx'
 import Github, {githubInfoLoader} from './components/Github/Github.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import TermsConditions from './components/Terms and condtions/TermsConditions.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />}/>
+      <Route path='termsconditions' element={<TermsConditions />}/>
+
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />}/>
       <Route path='user/' element={<User />} >
